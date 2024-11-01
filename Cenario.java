e381da1f13ddf814e3310f63d49037db97d42c55
Fernandohka <fernando2005andrade@gmail.com>
2024-11-01 00:44:42 -0300

    uns bagulho inutil

--- Cenario.java
+++ Cenario.java
@@ -1,3 +1,31 @@
 public class Cenario {
-    
+    String[] cenario;
+
+    public Cenario(String[] cenario){
+        this.cenario = cenario;
+    }
+
+    String[] cenarioAtual;
+
+    public String[] getCenarioAtual() {
+        return cenarioAtual;
+    }
+
+    public void setCenarioAtual(String[] cenarioAtual) {
+        this.cenarioAtual = cenarioAtual;
+    }
+
+    public void resetCenario(){
+        cenarioAtual = cenario;
+    }
+
+    public void desenharNoCenario(String desenho, int x, int y, int xTamanho, int yTamanho){
+        for (int i = x; i < x + xTamanho; i++) {
+            for (int j = y; j < y + yTamanho; j++) {
+                
+            }
+        }
+    }
+
+
 }
